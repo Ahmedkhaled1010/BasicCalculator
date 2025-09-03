b132c774d6d4c6740d69f905617408fad8ff2262
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-09-03 13:07:49 +0300

    Initialize the Operation Class That Responsible for Check Operation

---
+++ BasicCalculator/Classes/Helper.cs
@@ -0,0 +1,31 @@
+﻿using BasicCalculator.Classes.Operations;
+using BasicCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Classes
+{
+    public class Helper 
+    {
+        public static IEquetion checkOperation(char operation)
+        {
+            switch(operation)
+            {
+
+                case '+':
+                    return new Addition();
+                case '-':
+                    return new Subtraction();
+                case '*':
+                    return new Multiplication();
+                case '/':
+                    return new Division();
+                default:
+                    throw new InvalidOperationException("Operation not supported");
+                }
+            }
+    }
+}
---
+++ BasicCalculator/Classes/Operation.cs
@@ -0,0 +1,33 @@
+﻿using BasicCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Classes
+{
+    public class Operation : IOperation
+    {
+        private readonly Dictionary<char, IEquetion> operations;
+
+        public Operation() { 
+        
+            operations = new Dictionary<char, IEquetion>();
+        }
+
+        public IEquetion checkContainOperation(char operation)
+        {
+            if (operations.ContainsKey(operation))
+            {
+                return operations[operation];
+            }
+            else
+            {
+                IEquetion newOperation = Helper.checkOperation(operation);
+                operations.Add(operation, newOperation);
+                return newOperation;
+            }
+        }
+    }
+}
