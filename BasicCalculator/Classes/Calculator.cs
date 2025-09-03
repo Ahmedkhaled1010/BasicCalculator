9c96893132657c00ee82b749d9e202c4c11231e7
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-09-03 13:12:12 +0300

    Initilaize Calculator Class Responsible for Execute Calculation

---
+++ BasicCalculator/Classes/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using BasicCalculator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Classes
+{
+    public class Calculator : ICalculator
+    {
+        private readonly Operation operations;
+        public Calculator()
+        {
+            operations = new Operation();
+        }
+        public double Calculate(double a, double b, char sympol)
+        {
+            IEquetion operation = operations.checkContainOperation(sympol);
+            return operation.Execute(a, b);
+        }
+    }
+}
